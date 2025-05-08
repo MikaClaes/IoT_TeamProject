84d4770a1f429098e736f3af50ef5ced3a60c305
Mika Claes <claesmika@gmail.com>
2025-05-08 09:12:55 +0200

    added TODO

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-#Main code
-#test2
+#Functions
+
+#Variabelen
+
+#TODO: Lichtsensor
+
+#TODO: Temp sensor
+
+#TODO: LED-bar
+
+#TODO: Pomodoro Timer
+
+#TODO: Motor (zandloper)
+
+#TODO: Knop
+
+#TODO: Thingspeak
+
+#TODO: Display
+
+#TODO: AI Speech commands
